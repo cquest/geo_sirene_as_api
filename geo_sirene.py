d35461b7dd6714213d95a081702dafcebe7d582f
cquest <cquest@openstreetmap.fr>
2020-10-12 09:29:41 +0200

    ajout bbox et refactoring

--- geo_sirene.py
+++ geo_sirene.py
@@ -14,8 +14,9 @@ class geo_sirene(object):
     def getSIRET(self, req, resp):
         cur = db.cursor()
 
-        where = ''
+        where = b''
         where2 = b''
+        query = None
 
         siret = req.params.get('siret', None)
         if siret and len(siret) == 14:
@@ -27,40 +28,36 @@ class geo_sirene(object):
 
         ape = req.params.get('ape', None)
         if ape:
-            where2 = where2 + cur.mogrify(
-                ' AND activiteprincipaleetablissement LIKE %s ', (ape+'%',))
+            where2 = where2 + cur.mogrify( ' AND activiteprincipaleetablissement LIKE %s ', (ape+'%',))
 
-        lat = req.params.get('lat', None)
-        lon = req.params.get('lon', None)
-        dist = min(int(req.params.get('dist', 500)), 1000)
+        # recherche dans une bbox (xmin, ymin, xmax, ymax)
+        bbox = req.params.get('bbox', None)
+        if bbox:
+            box = bbox.split(',')
+            box2d = "ST_GeomFromText('LINESTRING(%f %f,%f %f)')" % (float(box[0]), float(box[1]), float(box[2]), float(box[3]))
+            where = where + cur.mogrify(' AND geom && '+box2d)
+        else:
+            # recherche par centre/rayon géo (lat/lon/dist)
+            lat = req.params.get('lat', None)
+            lon = req.params.get('lon', None)
+            dist = min(int(req.params.get('dist', 500)), 1000)
+            if lat and lon:
+                where = where + cur.mogrify("""
+        AND st_buffer(st_setsrid(st_makepoint(%s, %s),4326)::geography, %s)::geometry && geom
+        AND ST_DWithin(st_setsrid(st_makepoint(%s, %s),4326)::geography, geom::geography, %s)""",
+                            (lon, lat, dist, lon, lat, dist))
 
-        if lat and lon:  # recherche géographique
+        if where != '':
             query = cur.mogrify("""
 select json_build_object('source', %s, 'derniere_maj', %s, 'licence', %s,
     'nb_resultats', count(r),
     'type','Featurecollection',
     'features', case when count(*)=0 then array[]::json[] else array_agg(json_build_object('type','Feature',
-                                            'properties',json_strip_nulls(row_to_json(r)),
-                                            'geometry',st_asgeojson(geom,6,0)::json)) end )::text
-from (select coalesce(denominationusuelleetablissement,enseigne1etablissement, denominationunitelegale, denominationusuelle1unitelegale) as name, * from siret natural join siren
-where st_buffer(st_setsrid(st_makepoint(%s, %s),4326)::geography, %s)::geometry && geom
-    AND ST_DWithin(st_setsrid(st_makepoint(%s, %s),4326)::geography, geom::geography, %s)
-    AND etatadministratifetablissement='A'
-""", (SOURCE, DERNIER_MILLESIME, LICENCE, lon, lat, dist, lon, lat, dist)) + where2 + b') r'
-        else:
-            query = None
+        'properties',json_strip_nulls(row_to_json(r)),
+        'geometry',st_asgeojson(geom,6,0)::json)) end )::text
+from (select coalesce(denominationusuelleetablissement,enseigne1etablissement, denominationunitelegale, denominationusuelle1unitelegale) as name, * from siret join siren on (siret.siren=siren.siren)
+        where etatadministratifetablissement='A' """, (SOURCE, DERNIER_MILLESIME, LICENCE)) + where + where2 + b" ) r"
 
-        if query or where != '':
-            if not query:
-                query = cur.mogrify("""
-select json_build_object('source', %s, 'derniere_maj', %s, 'licence', %s,
-    'nb_resultats', count(r),
-    'resultats',array_to_json(array_agg(r)))::text
-from (select coalesce(denominationusuelleetablissement,enseigne1etablissement, denominationunitelegale, denominationusuelle1unitelegale) as name, * from siret natural join siren
-        where etatadministratifetablissement='A' """ + where.decode('utf8') + where2.decode('utf8') + """ ) r
-""", (SOURCE, DERNIER_MILLESIME, LICENCE))
-
-            print(query.decode())
             cur.execute(query)
             out = cur.fetchone()
 
